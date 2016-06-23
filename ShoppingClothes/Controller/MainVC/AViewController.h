89c4ad213ff7f40a355612eb6d9a31fcb6cee529
lyckite <lyckite@126.com>
2016-06-23 21:40:12 +0800

    456
    456

---
+++ ShoppingClothes/Controller/MainVC/AViewController.h
@@ -0,0 +1,13 @@
+//
+//  AViewController.h
+//  ShoppingClothes
+//
+//  Created by student on 16/6/23.
+//  Copyright © 2016年 jing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AViewController : UIViewController
+
+@end
